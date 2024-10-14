eb4a376146b428220550a046ec65c61b72f35fa3
Harshit Jain <121855889+HarshitJain1924@users.noreply.github.com>
2024-10-14 14:59:32 +0530

    Time: 34 ms (77.81%) | Memory: 17.3 MB (67.30%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -1,45 +1,22 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* temp=headA;
-        unordered_map<ListNode*,int> mp;
-        while(temp!=NULL){
-            mp[temp]++;
-            temp=temp->next;
-        }
-        temp=headB;
-        ListNode* ans=NULL;
-        while(temp!=NULL){
-            mp[temp]++;
-            if(mp[temp]==2){
-                ans=temp;
-                break;
-            }
-            temp=temp->next;
-        }
-        return ans;
-    }
+	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+		ListNode *ptr1 = headA;
+		ListNode *ptr2 = headB;
+		while(ptr1 != ptr2){
+			if(ptr1 == NULL){
+				ptr1 = headB;
+			}
+			else{
+				ptr1 = ptr1 -> next;
+			}
+			if(ptr2 == NULL){
+				ptr2 = headA;
+			}
+			else{
+				ptr2 = ptr2 -> next;
+			}
+		}
+		return ptr1;
+	}
 };
-int init = []
-{
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::ofstream out("user.out");
-    for(string s; getline(std::cin, s);)
-    {
-        if(s[0] != '0') out << "Intersected at '" << s << "'\n";
-        else out << "No intersection\n";
-        for(int i = 0; i < 4; ++i) getline(std::cin, s);
-    }
-    out.flush();
-    exit(0);
-    return 0;
-}();
